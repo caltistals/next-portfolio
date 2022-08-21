5d7431ceb47af814144905b632a32f07c29d444b
caltistals <y.db.qp.y@gmail.com>
2022-08-21 10:09:36 +0900

    Add: Components、NotionAPIを叩く関数を追加

---
+++ components/Navbar.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Link from "next/link";
+import { siteConfig } from "../site.config";
+
+const Navbar = () => {
+  return (
+    <nav className="relative flex flex-wrap items-center justify-between w-full w-screen py-3 text-gray-500 hover:text-gray-700 focus:text-gray-700 navbar navbar-expand-lg navbar-light">
+      <div className="flex flex-wrap items-center justify-between w-full px-6 container-fluid">
+        <div
+          className="w-full rounded-md bg-grey-light"
+          aria-label="breadcrumb"
+        >
+          <Link href="/">
+            <a className="text-gray-500 hover:text-gray-600">
+              {siteConfig.title}
+            </a>
+          </Link>
+          {/* Breadcrumb */}
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ site.config.ts
@@ -0,0 +1,6 @@
+export const siteConfig = {
+  title: "Caltistals",
+  twitterUrl: "",
+  instagramUrl: "",
+  githubUrl: "https://github.com/caltistals",
+};
