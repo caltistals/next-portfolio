f577392ccfaa54867cf84c1156f9296856056e58
caltistals <y.db.qp.y@gmail.com>
2022-09-20 23:47:20 +0900

    Fix: notionの画像を読み込めるように修正

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
-module.exports = {
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack"],
-    });
-    return config;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ["www.notion.so", "images.unsplash.com"],
   },
 };
+
+module.exports = nextConfig;
