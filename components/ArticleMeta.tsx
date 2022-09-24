f8d0ad1c28306e695f84ed01ac304ebba211e623
caltistals <y.db.qp.y@gmail.com>
2022-09-24 15:28:22 +0900

    Add: 記事詳細ページの追加

---
+++ components/ArticleMeta.tsx
@@ -0,0 +1,59 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { ArticleMetaProps } from "../types/types";
+import { getCover, getDate, getMultiSelect, getText } from "../utils/property";
+
+const ArticleMeta: React.FC<ArticleMetaProps> = ({ page }) => {
+  return (
+    <>
+      {/* page cover */}
+      <Image
+        className="w-full max-w-screen-lg my-4 rounded-lg aspect-video"
+        src={getCover(page.cover)}
+        alt=""
+        objectFit="cover"
+        width={640}
+        height={360}
+        quality={50}
+      />
+
+      {/* page name */}
+      <h1 className="my-8 text-3xl font-bold">
+        {getText(page.properties.name.title)}
+      </h1>
+      <div className="px-6 py-4 text-sm text-gray-500 bg-gray-100 rounded">
+        <div className="grid grid-cols-3 gap-4">
+          {/* published */}
+          <div className="col-span-1">Published</div>
+          <div className="col-span-2">
+            {getDate(page.properties.published.date)}
+          </div>
+
+          {/* author */}
+          <div className="col-span-1">Author</div>
+          <div className="col-span-2">
+            {getText(page.properties.author.rich_text)}
+          </div>
+
+          {/* tags */}
+          <div className="col-span-1">Tags</div>
+          <div className="col-span-2">
+            {/* change later */}
+            {getMultiSelect(page.properties.tags.multi_select).map(
+              (tag: string, index: number) => (
+                <Link key={index} href={`/tags/${tag}`}>
+                  <a className="mr-3 text-gray-700 no-underline border-b border-gray-700 border-solid opacity-70">
+                    <span>{`#${tag}`}</span>
+                  </a>
+                </Link>
+              )
+            )}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ArticleMeta;
---
+++ pages/articles/[slug].tsx
@@ -0,0 +1,64 @@
+import { GetStaticPaths, GetStaticProps, NextPage } from "next";
+import React from "react";
+import ArticleMeta from "../../components/ArticleMeta";
+import Layout from "../../components/Layout";
+import { ArticleProps, Params } from "../../types/types";
+import { fetchBlocksByPageId, fetchPages } from "../../utils/notion";
+import { getText } from "../../utils/property";
+import NotionBlocks from "notion-block-renderer";
+import MetaData from "../../components/MetaData";
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const { results } = await fetchPages({ isToppage: false });
+  const paths = results.map((page: any) => {
+    return {
+      params: {
+        slug: getText(page.properties.slug.rich_text),
+      },
+    };
+  });
+  return {
+    paths: paths,
+    fallback: "blocking", // プリレンダリングをブロッキング
+  };
+};
+
+// 各リクエストごとにページをレンダリングする
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  const { slug } = ctx.params as Params;
+
+  const { results } = await fetchPages({ slug: slug, isToppage: false });
+  const page = results[0];
+  const pageId = page.id;
+  const { results: blocks } = await fetchBlocksByPageId(pageId);
+
+  return {
+    props: {
+      page: page, // 記事のプロパティ
+      blocks: blocks, // 記事の本体
+    },
+    revalidate: 10, // ISR
+  };
+};
+
+// PageなのでNextPage型にする
+const Article: NextPage<ArticleProps> = ({ page, blocks }) => {
+  return (
+    <Layout>
+      <MetaData title={`${getText(page.properties.name.title)}`} />
+      <article className="flex flex-col items-center justify-center">
+        {/* meta section */}
+        <div className="my-12">
+          <ArticleMeta page={page} />
+        </div>
+
+        {/* article */}
+        <div className="my-12">
+          <NotionBlocks blocks={blocks} isCodeHighlighter={true} />
+        </div>
+      </article>
+    </Layout>
+  );
+};
+
+export default Article;
