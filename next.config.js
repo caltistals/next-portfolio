55d0ad407d4e63351c2cdfed137ed763598b7f82
caltistals <y.db.qp.y@gmail.com>
2022-09-25 02:34:36 +0900

    Fix: imagesのdomainsを修正

--- next.config.js
+++ next.config.js
@@ -2,7 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["www.notion.so", "images.unsplash.com"],
+    domains: [
+      "www.notion.so",
+      "images.unsplash.com",
+      "secure.notion-static.com",
+      "s3.us-west-2.amazonaws.com",
+    ],
   },
 };
 
