ee978f4a75f3ee079350cd1de64c5d2757a411c0
caltistals <y.db.qp.y@gmail.com>
2022-09-24 15:27:35 +0900

    Fix: ArticleCardのスタイルを修正

--- components/ArticleCard.tsx
+++ components/ArticleCard.tsx
@@ -22,7 +22,7 @@ const ArticleCard: FC<ArticleCardProps> = ({ page }) => {
           </div>
           {/* title & date*/}
           <div className="px-6 pt-4 ">
-            <h2 className="mb-3 text-base font-medium group-hover:text-blue-500">
+            <h2 className="mb-3 text-base font-bold group-hover:text-blue-500">
               {getText(page.properties.name.title)}
             </h2>
             <p className="text-xs text-gray-700">
@@ -34,12 +34,15 @@ const ArticleCard: FC<ArticleCardProps> = ({ page }) => {
           <div className="px-6 pb-4 ">
             {getMultiSelect(page.properties.tags.multi_select).map(
               (tag, index) => (
-                <span
-                  key={index}
-                  className="px-2 py-1 mb-2 mr-2 text-sm font-normal break-words bg-gray-200 rounded-lg hover:text-blue-500"
-                >
-                  {`#${tag}`}
-                </span>
+                <Link href={`/tags/${tag}`} key={index}>
+                  <object className="inline-block ">
+                    <a>
+                      <span className="px-2 py-1 mb-2 mr-2 text-sm font-normal break-words bg-gray-200 rounded-lg hover:text-blue-500">
+                        {`#${tag}`}
+                      </span>
+                    </a>
+                  </object>
+                </Link>
               )
             )}
           </div>
