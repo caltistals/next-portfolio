e85097f319dc57ac8bb5f7dc2e43c613a06547dd
caltistals <y.db.qp.y@gmail.com>
2022-09-27 03:35:34 +0900

    Update: og:imageを設定

--- components/MetaData.tsx
+++ components/MetaData.tsx
@@ -3,6 +3,7 @@ import { MetaDataProps } from "../types/types";
 import React from "react";
 
 const MetaData: React.FC<MetaDataProps> = ({ title }) => {
+  const forOg: string = title ? title : "caltistals.dev";
   return (
     <Head>
       <title>{title ? `${title} | caltistals.dev` : `caltistals.dev`}</title>
@@ -19,6 +20,10 @@ const MetaData: React.FC<MetaDataProps> = ({ title }) => {
         content="caltistalsのブログです。技術やくだらないことについて書いています。"
       />
       <meta property="og:site_name" content="caltistals.dev" />
+      <meta
+        property="og:image"
+        content={`https://og-image-caltistals.vercel.app/${forOg}.png?theme=light&md=1&fontSize=100px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fvercel-triangle-black.svg`}
+      />
     </Head>
   );
 };
