a51def24737e6b9e232b9984d2849fee233f17ae
caltistals <y.db.qp.y@gmail.com>
2022-09-27 15:46:02 +0900

    Clean: MetaDataをリファクタリング

--- components/MetaData.tsx
+++ components/MetaData.tsx
@@ -3,18 +3,15 @@ import { MetaDataProps } from "../types/types";
 import React from "react";
 
 const MetaData: React.FC<MetaDataProps> = ({ title }) => {
-  const forOg: string = title ? title : "caltistals.dev";
+  const metaTitle: string = title ? title : "caltistals.dev";
   return (
     <Head>
-      <title>{title ? `${title} | caltistals.dev` : `caltistals.dev`}</title>
+      <title>{metaTitle}</title>
       <meta
         property="description"
         content="caltistalsのブログです。技術やくだらないことについて書いています。"
       />
-      <meta
-        property="og:title"
-        content={title ? `${title} | caltistals.dev` : `caltistals.dev`}
-      />
+      <meta property="og:title" content={metaTitle} />
       <meta
         property="og:description"
         content="caltistalsのブログです。技術やくだらないことについて書いています。"
@@ -22,7 +19,7 @@ const MetaData: React.FC<MetaDataProps> = ({ title }) => {
       <meta property="og:site_name" content="caltistals.dev" />
       <meta
         property="og:image"
-        content={`https://og-image-caltistals.vercel.app/${forOg}.png?theme=light&md=1&fontSize=100px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fvercel-triangle-black.svg`}
+        content={`https://og-image-caltistals.vercel.app/${metaTitle}.png?theme=light&md=1&fontSize=100px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fvercel-triangle-black.svg`}
       />
     </Head>
   );
