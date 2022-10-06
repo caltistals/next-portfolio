bbc0418067069e9355097a9cd6eff9f9d1d88372
caltistals <y.db.qp.y@gmail.com>
2022-10-06 23:11:45 +0900

    [update] 全体の背景色を変更

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -4,7 +4,7 @@ import Navbar from "./Navbar";
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
-    <div className="relative mb-0">
+    <div className="relative mb-0 bg-pale-yellow-orange">
       <div className="flex flex-col md:flex-row">
         <Navbar />
         <main className="flex-1 px-4 pb-12 mt-12 md:mt-0">{children}</main>
