9380962fa2e33aec66f390fc32408f4aa2e39724
caltistals <y.db.qp.y@gmail.com>
2022-12-09 03:32:40 +0900

    add: internHistory

---
+++ components/Intern/intern-history.ts
@@ -0,0 +1,16 @@
+import { CareerProps } from "./Career.type";
+
+export const internHistory: CareerProps[] = [
+  {
+    companyName: "フェンリル株式会社",
+    role: "ウェブエンジニアコース",
+    fromDate: "2022/09/05",
+    untilDate: "2022/09/09",
+  },
+  {
+    companyName: "チームラボ株式会社",
+    role: "Webアプリエンジニア",
+    fromDate: "2022/08/22",
+    untilDate: "2022/09/02",
+  },
+];
